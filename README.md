d990f4aa1256ef6ef542322e4378ad211d41f050
Menko <menko@hornstein.me>
2025-05-25 09:22:55 -0700

    Enhance service worker functionality and update manifest for PWA support

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab8-Starter
+How are graceful degradation and service workers related?
+Service workers support a website by enabling features like offline access and cached content. So if the network is slow or unavailable, the service worker can help fulfill the RAIL specifications by serving the cached version of a page. This ensures the user can still access the site even if mordern capabilities are missing - which is the core idea of graceful degragation
--- sw.js
+++ sw.js
@@ -9,7 +9,14 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      return cache.addAll([
+        'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+    ]);
     })
   );
 });
@@ -32,6 +39,20 @@ self.addEventListener('fetch', function (event) {
   //       fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
   /*******************************/
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (cachedResponse) {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request).then(function (response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+    }
+  )
+)
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
   // B8. TODO - If the request is in the cache, return with the cached version.
